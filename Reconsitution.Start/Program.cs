1d22eb46a50381a76daff6f4322ee07eb6e6b311
Yanlin Liu <lyLiu@99-6-172-208.lightspeed.hstntx.sbcglobal.net>
2018-08-08 17:40:34 +0800

    初始化项目

---
+++ Reconsitution.Start/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Reconsitution.Start
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
