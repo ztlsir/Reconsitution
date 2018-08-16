4985b8ad373b1c127c77d3e4a2ce99a4a09a4edc
ztlsir <584821699@qq.com>
2018-08-16 17:41:14 +0800

    运用多态取代与价格相关的条件逻辑：迁移Movie.GetCharge()中业务逻辑到Price.GetCharge()中

--- Refactoring.Start/Movie.cs
+++ Refactoring.Start/Movie.cs
@@ -43,26 +43,7 @@ namespace Refactoring.Start
 
         public double GetCharge(int daysRented)
         {
-            double result = 0;
-            //determine amounts for each line
-            switch (this.PriceCode)
-            {
-                case Movie.REGULAR:
-                    result += 2;
-                    if (daysRented > 2)
-                        result += (daysRented - 2) * 1.5;
-                    break;
-                case Movie.NEW_RELEASE:
-                    result += daysRented * 3;
-                    break;
-                case Movie.CHILDRENS:
-                    result += 1.5;
-                    if (daysRented > 3)
-                        result += (daysRented - 3) * 1.5;
-                    break;
-            }
-
-            return result;
+            return this.price.GetCharge(daysRented);
         }
 
         public int GetFrequentRenterPoints(int daysRented)
